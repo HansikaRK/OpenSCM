cde11916109d149255b4b9d4c995a8eeece38332
Ushan Savindu <ushansavindu666@gmail.com>
2025-08-12 17:53:00 +0530

    Initial commit with the folder structure

---
+++ ApiGateway/src/test/java/com/openSCM/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openSCM.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
