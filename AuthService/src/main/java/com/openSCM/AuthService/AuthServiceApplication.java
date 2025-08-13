64ff74386cb823d88fcb10ad050e15dd347c77e0
Ushan Savindu <ushansavindu666@gmail.com>
2025-08-13 22:23:31 +0530

    Services are registered in the eureka server

--- AuthService/src/main/java/com/openSCM/AuthService/AuthServiceApplication.java
+++ AuthService/src/main/java/com/openSCM/AuthService/AuthServiceApplication.java
@@ -3,7 +3,11 @@ package com.openSCM.AuthService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Remove the below import when database are used
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//Remove the exlude part when the databases are used
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
