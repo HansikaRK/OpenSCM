995c7d703b7993a924ada29ea75f52d528c722a7
Ushan Savindu <ushansavindu666@gmail.com>
2025-08-14 18:22:18 +0530

    Set up eureka server for the service registry and succesfully registered the services. But docker is not tested

--- AuthService/src/main/java/com/openSCM/AuthService/AuthServiceApplication.java
+++ AuthService/src/main/java/com/openSCM/AuthService/AuthServiceApplication.java
@@ -2,12 +2,14 @@ package com.openSCM.AuthService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //Remove the below import when database are used
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //Remove the exlude part when the databases are used
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
